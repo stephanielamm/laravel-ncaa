e448fdff58821c912ececcbb9a908cad87d90f59
stephanielamm <slamm5@live.unc.edu>
2017-03-28 18:29:18 -0400

    Examples of all functions you would use.

--- laravel-ncaa-api/app/Http/Controllers/GameController.php
+++ laravel-ncaa-api/app/Http/Controllers/GameController.php
@@ -11,8 +11,29 @@ use App\Http\Requests;
 
 class GameController extends Controller
 {
-    public function foo()
-    {
-      return Response::json('Stephanie');
-    }
+  public function index ()
+  {
+    // GET /games
+    // lists all games
+  }
+  public function create ()
+  {
+    // POST /games
+    // creates new game
+  }
+  public function show ($id)
+  {
+    // GET /games/$id
+    // shows a single game
+  }
+  public function update ($id)
+  {
+    // PUT /games/$id
+    // updates a single game
+  }
+  public function destroy ($id)
+  {
+    // DELETE /games/$id
+    // removes a single game
+  }
 }
